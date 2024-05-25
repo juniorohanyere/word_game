4568df14f6c812e7ea718ddee90f54009294523d
Junior Ohanyere <junohanyere@gmail.com>
2024-05-25 14:16:07 +0100

    documentation for isnumber function

--- validate.c
+++ validate.c
@@ -1,5 +1,14 @@
 #include <ctype.h>
 
+/**
+ * isnumber - checks if input is number
+ *
+ * @str: the input to check
+ *
+ * Return: return 0 if @str is not number
+ *	   return 1 otherwise
+*/
+
 int isnumber(char *str)
 {
 	int i;
