d116f119e66b0b6588cde09bdd630900e47d7bbc
Junior Ohanyere <junohanyere@gmail.com>
2024-06-01 13:50:58 +0000

    moved content of validate.* to tests/main.c

--- test/main.c
+++ test/main.c
@@ -5,6 +5,31 @@
 #include "../wordg.h"
 
 /**
+ * isnumber - checks if input is number
+ *
+ * @str: the input to check
+ *
+ * Return: return 0 if @str is not number
+ *	   return 1 otherwise
+*/
+
+int isnumber(char *str)
+{
+	int i;
+
+	if (str[0] == '\0')
+		return (0);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (isdigit(str[i] == 0)
+			return (0);
+	}
+
+	return (1);
+}
+
+/**
  * main - entry point
  *
  * Description: a word game program
--- validate.h
+++
@@ -1,6 +0,0 @@
-#ifndef __VALIDATE_H
-#define __VALIDATE_H
-
-int isnumber(char *str);
-
-#endif	/* __VALIDATE_H */
